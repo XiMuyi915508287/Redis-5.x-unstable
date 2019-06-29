c708ce0ff5f8b6c2587875d619185f89f05b07e3
chenjingjun <chenjingjun@aobi.com>
2019-06-29 20:02:11 +0800

    redis sharding

--- shared-test/shared-server.bat
+++ shared-test/shared-server.bat
@@ -1,5 +1,5 @@
 set redispath=E:/software/Redis-5.x-unstable
-set workspace=%redispath%/single-test/7208
+set workspace=%redispath%/shared-test/%1%
 cd %workspace%
 %redispath%/redis-server.exe %workspace%/redis.conf
 pause
---
+++ shared-test/start-all-server.bat
@@ -0,0 +1,1 @@
+for %%i in (7108,7109,7110) do start shared-server.bat %%i
